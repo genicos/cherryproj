26bd0e8862f20828c8330d229a647357f4822b3c
Alyssa Pratt <alyssa@zpratts.net>
2024-12-03 12:58:25 -0800

    update contact matrix to match CherryML

--- create_contact_matrix.py
+++ create_contact_matrix.py
@@ -6,17 +6,20 @@ pdb_code = sys.argv[1]
 pdb_file = sys.argv[2]
 
 def calc_dist_matrix(structure):
-    atoms = [a for a in structure.get_atoms() if a.get_name() == "CA"] # carbon alpha atoms
+    atoms = [a for a in structure.get_atoms() if a.get_name() == "CA"] # alpha carbons
     n_atoms = len(atoms)
     dist_matrix = np.zeros((n_atoms, n_atoms))
     for i in range(n_atoms):
         for j in range(i + 1, n_atoms):
-            dist = atoms[i] - atoms[j]
+            if abs(i-j) > 7:
+                dist = np.inf # disregard closeness of contacts that are within 7 amino acids of each other in primary sequence
+            else:
+                dist = atoms[i] - atoms[j]
             dist_matrix[i, j] = dist
             dist_matrix[j, i] = dist
     return dist_matrix
 
-def contact_matrix(dist_matrix, threshold=12.0):
+def contact_matrix(dist_matrix, threshold=8.0): #8 angstrom threshold
     return dist_matrix < threshold
 
 pdb_parser = Bio.PDB.PDBParser()
