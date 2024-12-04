aa39d0bf02367bfe01eef986e314293d8d01d942
Alyssa Pratt <alyssa@zpratts.net>
2024-12-03 18:46:16 -0800

    add contact matrices

--- create_contact_matrix.py
+++ create_contact_matrix.py
@@ -1,12 +1,21 @@
-import Bio.PDB, sys
+import Bio.PDB, sys, os
 import numpy as np
+from pathlib import Path
 
-# usage: <PDB code> <pdb file path>"
-pdb_code = sys.argv[1]
-pdb_file = sys.argv[2]
+# usage: <pdb directory path> <contact matrix output path>
+pdb_dir = sys.argv[1]
+output_dir = sys.argv[2]
 
-def calc_dist_matrix(structure):
-    atoms = [a for a in structure.get_atoms() if a.get_name() == "CA"] # alpha carbons
+def calc_dist_matrix(structure, chain_id=None):
+    if chain_id is not None:
+        chains = [chain for chain in structure.get_chains() if chain.id == chain_id]
+        if len(chains) != 1:
+            print(f"{len(chains)} chains matching {chain_id}, 1 is required")
+            exit()
+        else:
+            atoms = [a for a in chains[0].get_atoms() if a.get_name() == "CA"] # alpha carbons
+    else:
+        atoms = [a for a in structure.get_atoms() if a.get_name() == "CA"] # alpha carbons
     n_atoms = len(atoms)
     dist_matrix = np.zeros((n_atoms, n_atoms))
     for i in range(n_atoms):
@@ -19,17 +28,26 @@ def calc_dist_matrix(structure):
             dist_matrix[j, i] = dist
     return dist_matrix
 
-def contact_matrix(dist_matrix, threshold=8.0): #8 angstrom threshold
+def contact_matrix(dist_matrix, threshold=8): #8 angstrom threshold
     return dist_matrix < threshold
 
 pdb_parser = Bio.PDB.PDBParser()
-structure = pdb_parser.get_structure(pdb_code, pdb_file)
-
-# Calculate distance matrix
-dist_matrix = calc_dist_matrix(structure)
-
-# Create contact matrix
-contact_matrix = contact_matrix(dist_matrix).astype(int)
 
 
-print(contact_matrix)
+pdb_files = []
+for root,dirs,files in os.walk(pdb_dir):
+    for name in files:
+        if name.endswith(('pdb')): 
+            pdb_files.append(os.path.join(root,name))
+
+Path(output_dir).mkdir(parents=True, exist_ok=True)
+for pdb_file in pdb_files:
+    id = str(Path(os.path.basename(pdb_file)).stem).split("_")[0]
+    structure = pdb_parser.get_structure(id, pdb_file)
+    # Calculate distance matrix
+    dist_matrix = calc_dist_matrix(structure)
+    # Create contact matrix
+    result_matrix = contact_matrix(dist_matrix).astype(int)
+    matrix_path = os.path.join(output_dir,str(Path(os.path.basename(pdb_file)).stem) + ".txt") 
+    print(matrix_path)
+    np.savetxt(matrix_path, result_matrix, header=id, fmt = '%d')
