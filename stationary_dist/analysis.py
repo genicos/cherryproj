0a966923b149b32bc60d7542eb1a77f89cb7fc80
genicos <nicoatnico.com@gmail.com>
2024-12-03 17:52:00 -0800

    distribution

--- stationary_dist/analysis.py
+++ stationary_dist/analysis.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def calc_stationary_dist_from_rate_matrix_file(rate_matrix_file):
-    matrix = np.zeros((20, 20))
+    Q = np.zeros((20, 20))
 
     with open(rate_matrix_file) as f:
         lines = f.readlines()
@@ -14,9 +14,21 @@ def calc_stationary_dist_from_rate_matrix_file(rate_matrix_file):
         for i in range(1,len(lines)):
             line = lines[i].split()[1:]
             for j in range(20):
-                matrix[i-1][j] = float(line[j])
+                Q[i-1][j] = float(line[j])
     
-    print(matrix)
+    print(Q)
+
+    Q_T = Q.T
+
+    # Add the normalization condition
+    A = np.vstack([Q_T[:-1], np.ones(Q.shape[1])])  # Replace one equation with sum(pi) = 1
+    b = np.zeros(Q.shape[0])
+    b[-1] = 1  # Corresponds to sum(pi) = 1
+
+    # Solve the system
+    pi = np.linalg.lstsq(A, b, rcond=None)[0]
+
+    print("Stationary distribution:", pi)
 
 
 
